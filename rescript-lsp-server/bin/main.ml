99a219e0f0ecc2e95f9ba01c8a15701b8a01d049
Pedro Castro <aspeddro@gmail.com>
2023-03-17 22:26:35 -0300

    rename lib name

--- rescript-lsp-server/bin/main.ml
+++ rescript-lsp-server/bin/main.ml
@@ -1,3 +1,17 @@
-
 let () =
-  Rescript_lsp_server.Server.run ()
+  let usage = "rescriptlsp" in
+
+  let args =
+    [
+      ( "--version",
+        Arg.Unit
+          (fun _ ->
+            prerr_endline "beta";
+            exit 0),
+        "Display version" );
+    ]
+  in
+
+  Arg.parse args (fun err -> raise @@ Arg.Bad ("Unknow option: " ^ err)) usage;
+
+  Rescript_language_server.Server.run ()
